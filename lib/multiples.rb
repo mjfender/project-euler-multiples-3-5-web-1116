b470ca95159a983a35b876d763cdb6595dd2c1ed
Matt Fender <mjfender@gmail.com>
2017-01-04 09:35:49 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+def collect_multiples(number)
+  max = number
+  nums = [3, 5]
+  multiples_in_range = []
+
+  nums.each do |num|
+    i = 1
+    until num * i >= max do
+      multiples_in_range << num * i
+      i += 1
+    end
+  end
+  multiples_in_range.sort.uniq
+
+end
+
+def sum_multiples(limit)
+  sum = 0
+  collect_multiples(limit).each do |num|
+    sum += num
+  end
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,30 @@
-# Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+  def initialize(argument)
+    @limit = argument
+  end
+
+  def collect_multiples
+    nums = [3, 5]
+    multiples_in_range = []
+
+    nums.each do |num|
+      i = 1
+      until num * i >= limit do
+        multiples_in_range << num * i
+        i += 1
+      end
+    end
+    multiples_in_range.sort.uniq
+  end
+
+  def sum_multiples
+    sum = 0
+    self.collect_multiples.each do |num|
+      sum += num
+    end
+  sum
+  end
+
+
+end
